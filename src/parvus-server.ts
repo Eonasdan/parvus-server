5c614c6c927ee658c9bcb75b5ec217de788997ab
Jonathan Peterson <me@eonasdan.com>
2022-05-21 11:43:11 -0400

    * more error details * fixed bg color

--- src/parvus-server.ts
+++ src/parvus-server.ts
@@ -158,7 +158,7 @@ export default class ParvusServer {
             console.error(ex);
             res.setHeader('Content-Type', 'text/html');
             res.writeHead(500);
-            res.end(`<html lang="en"><body style="background-color: #171717;"><h1>Error</h1>Failed to load requested file at ${url}</body></html>`);
+            res.end(`<html lang="en"><body style="background-color: #171717;"><h1>Error</h1><p>Failed to load requested file at ${url}</p><p><pre>${ex}</pre></p></body></html>`);
         }
     }
 
